9887e555143756b68b183a6fdf5fed1d506025ab
akazh2511 <akashdeepvarshney2703@gmail.com>
2022-07-29 17:07:12 +0530

    dont know

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "https://how-z-doing.herokuapp.com/"; 
+const ENDPOINT = "http://localhost:5000/" || "https://how-z-doing.herokuapp.com/"; 
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
